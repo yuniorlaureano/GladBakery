4d8f5d767646fd2b8d6052096d2bfc19c2a98313
= <=>
2019-04-29 15:36:06 -0400

    Formulario de pasteles, formulario de ingreidentes, formulario de insumos

--- public/js/pastel.js
+++ public/js/pastel.js
@@ -8,4 +8,23 @@ $(function(){
     $("#btn-insumo").on("click", function(){
         $("#modal-pastel-insumo").modal("show");
     });
+
+    $("#tbn-producto").on("click", function(){
+        var context = $(this).data("context");
+        if(context == "listar"){
+            $(this).data("context", "crear");
+            $(this).text("Crear Producto");
+            $("#formulario-listar-producto").show();
+            $("#formulario-registro-producto").hide();
+        } else {
+            $(this).data("context", "listar");
+            $(this).text("Listar Producto");
+            $("#formulario-listar-producto").hide();
+            $("#formulario-registro-producto").show();
+        }
+    });
+
+    $("#upload-pastel").on("click", function(){
+        $("#upload-pastel-image").click();
+    });
 });
